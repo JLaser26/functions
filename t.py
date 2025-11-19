de2d6c10dc39a8e28bb52652e8bec07f3d87176d
Jai <jaikamath169@gmail.com>
2025-11-19 20:58:59 +0530

    creating stuff

--- t.py
+++ t.py
@@ -1,8 +1,14 @@
+#Work in progress!
+
 class function:
     def __init__(self, *vars):
+        """Add each variable as a string, for ex:- '-2x' """
         self.vars = vars
     
-    def functionality(self, vars):
+    def __str__(self):
+        return " ".join(self.vars)
+    
+    def functionality(self):
         return
     
     def output(self): return self.functionality()
@@ -15,9 +21,12 @@ class function:
     
     def number_of_variables(self): return len(self.vars)
 
-    def derivative(self) -> function:
+    def derivative(self):
         ...
     
-    def integral(self) -> function:
+    def integral(self):
         ...
 
+#testing
+y = function("x", "- 2y", "+ 3z")
+print(y, y.number_of_variables())
