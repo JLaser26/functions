b05e506d2f129ae247bfd28f05546a27a745d952
Jai <jaikamath169@gmail.com>
2025-11-20 17:47:03 +0530

    added evaluater

--- t.py
+++ t.py
@@ -8,8 +8,23 @@ class function:
     def __str__(self):
         return " ".join(self.vars)
     
+    def tokens(self): return list(self.vars)
+
+    #try to break the function into small parts
+    def evaluater(self):
+        tk = self.tokens()
+        evaluated_tokens = []
+        for i in tk:
+            for j in i:
+                try:
+                    x = eval(j)
+                except Exception:
+                    x = j
+                evaluated_tokens.append(x)
+        return evaluated_tokens
+    
     def functionality(self):
-        return
+        ...
     
     def output(self): return self.functionality()
 
@@ -28,5 +43,6 @@ class function:
         ...
 
 #testing
-y = function("x", "- 2y", "+ 3z")
+y = function("x", "- 2w", "+ 3z")
 print(y, y.number_of_variables())
+print(y.evaluater())
